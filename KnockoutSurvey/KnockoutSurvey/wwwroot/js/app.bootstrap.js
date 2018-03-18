952c9c8bb510220d7343077fa6c81b4456f36312
Luke Wickstead <lwickstead@googlemail.com>
2018-03-18 11:22:12 +0000

    Removing everyything unrequireed from the default template, added in third party javascript libraries and iniitiated require js bootstrapping

---
+++ KnockoutSurvey/KnockoutSurvey/wwwroot/js/app.bootstrap.js
@@ -0,0 +1,17 @@
+"use strict";
+requirejs.config({
+    paths: {
+        'jquery': '../lib/jquery/dist/jquery.min',
+    }
+});
+
+// Server Side
+// CSS improve
+// Footer
+// KO Templates
+// Test JavaScript
+// Final Check
+
+require(['jquery',],
+    function ($) {
+    });
